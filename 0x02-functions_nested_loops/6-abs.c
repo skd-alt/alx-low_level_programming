ae8046458dfff9bfe8166bdd18a586e14b92e2f8
skd-alt <sipho@skanalytics.co.za>
2023-03-21 13:39:37 -0700

    absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer
+ * @num: The integer to be computed
+ * Return: The absolute value
+ */
+int _abs(int num)
+{
+	if (num >= 0)
+		return (num);
+	else
+		return (-num);
+}
