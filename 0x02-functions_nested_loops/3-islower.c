4090e44030691af3365811d4227ec7ee083a912c
skd-alt <sipho@skanalytics.co.za>
2023-03-21 12:59:22 -0700

    islower check

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - checks if a character is lowercase
+ *
+ * Return: 1 if letter is lowercase, 0 if not
+ */
+
+int _islower(int ch)
+{
+	if (ch >= 'a' && ch <= 'z')
+		return (1);
+	else
+		return (0);
+}
