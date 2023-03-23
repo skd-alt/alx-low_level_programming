64815129bc7d1466d1dff0efadf524f030ec33f5
skd-alt <sipho@skanalytics.co.za>
2023-03-23 14:42:05 -0700

    print more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - print more numbers
+ *Return: returns nothing
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; 1++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j > 9)
+		{
+			_putchar((j / 10) + '0');
+		}
+		_putchar((j % 10) + '0');
+		}
+		_putchar(10);
+	}
+}
