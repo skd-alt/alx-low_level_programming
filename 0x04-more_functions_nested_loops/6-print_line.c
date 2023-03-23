663720b3602ca549c95b7dc0c84000bc1dee34e3
skd-alt <sipho@skanalytics.co.za>
2023-03-23 14:55:34 -0700

    Print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - prints straight line n times
+ *
+ * @n: times straight line is printed
+ * Return: no return
+ */
+
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		int i;
+
+		for (i = 0; i < n; i++)
+		{
+			_putchar(95);
+		}
+	}
+	
+	_putchar('\n');
+}
