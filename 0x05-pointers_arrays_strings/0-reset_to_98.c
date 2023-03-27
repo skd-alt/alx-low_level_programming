051372ebb73871ceb3be527542d9c1358b1fc8db
skd-alt <sipho@skanalytics.co.za>
2023-03-27 12:05:12 -0700

    Reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function that takes a pointer to an int
+ * and updates the value it points to 98
+ *
+ * @n: pointer to n to be updated
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
