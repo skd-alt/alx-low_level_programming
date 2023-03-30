836e6f18b8aa20589315bcd330c21a8be0595691
skd-alt <sipho@skanalytics.co.za>
2023-03-30 14:39:27 -0700

    compare s1 to s2

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcap - Function which compares two strings.
+ * _strcmp - Function which compares two strings.
  * @s1: first string
  * @s2: second string
  * Return:
@@ -12,26 +12,26 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int 1 = 0, diff = 0;
+	int i = 0, diff = 0;
 
-	while (1)
+	while (i >= 0)
 	{
-		if (s1[i] == '\0' && s2[i] == '\0')
+		if (s1[i] == '\0' && s2[i] != '\0')
+		{
 			break;
+		}
 		else if (s1[i] == '\0')
 		{
-			diff = s2[i];
+			diff = 15;
 			break;
 		}
 		else if (s2[i] == '\0')
 		{
-			diff = s1[i];
+			diff = -15;
 			break;
 		}
-		else if (s1[i] != s1[i])
-		{
-			break;
-		}
+		else if (s1[i] != s2[i])
+			diff = 15
 		else
 			i++;
 
