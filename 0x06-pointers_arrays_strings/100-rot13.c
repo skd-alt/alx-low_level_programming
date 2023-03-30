4cd4541c7c9e718618b0006e93f178c7fd7e6904
skd-alt <sipho@skanalytics.co.za>
2023-03-30 16:13:50 -0700

    rot 13 encode

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encoder rot13
+ * @s: pointer to string
+ * Return: *s
+ */
+
+char *rot13(char *s)
+{
+	int i;
+	int j;
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == alpha[j])
+			{
+				s[i] = rot13[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
