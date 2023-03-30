cb258409e53bc8908f2d1d69fcd22bc91dfec333
skd-alt <sipho@skanalytics.co.za>
2023-03-30 15:17:37 -0700

    Reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,10 +9,11 @@ void reverse_array(int *a, int n)
 {
 	int tmp, index, j;
 
-	index = n / 2
+	index = n / 2;
 	for (j = 0; j < index; j++)
 	{
-		tmp = a[j];
+		tmp = a[n - 1 - j];
 		a[n - 1 - j] = a[j];
 		a[j] = tmp;
+	}
 }
