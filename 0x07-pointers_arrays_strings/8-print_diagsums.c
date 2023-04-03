53f117841fcac7ee74d72a633d64f6a9799b7cf6
skd-alt <sipho@skanalytics.co.za>
2023-04-03 14:15:31 -0700

    add diagonal

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,10 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_diagsums - Edd diagonal Sum
+ * print_diagsums - Entry point
  * @a: input
  * @size: input
- *
  * Return: Always 0 (Success)
  */
 void print_diagsums(int *a, int size)
@@ -16,8 +15,11 @@ void print_diagsums(int *a, int size)
 
 	for (y = 0; y < size; y++)
 	{
-		sum1 = sum1 + a[y][y];
-		sum2 = sum2 + a[y][size - y - 1];
+		sum1 = sum1 + a[y * size + y];
+	}
+	for (y = size - 1; y >= 0; y--)
+	{
+		sum2 += a[y * size + (size - y - 1)];
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
