263619c238d614ba96640080182704c07cff03f0
skd-alt <sipho@skanalytics.co.za>
2023-04-10 14:04:58 -0700

    print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints name of program
+ * @argc: size of arg vector
+ * @agrv: arguement vector
+ *
+ * Return: Always 0 success
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Agrc and Argv Tutorial
