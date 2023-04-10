14dab8a96095259d43fc55e07bc3177ead52dfab
skd-alt <sipho@skanalytics.co.za>
2023-04-10 14:21:22 -0700

    Print all Args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alll arguements
+ * @argc: size of arg vector
+ * @argv: arguement vector
+ *
+ * Return: Always 0 success
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+	for(i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
