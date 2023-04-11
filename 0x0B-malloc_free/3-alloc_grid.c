3c111fb87864f26f84fd2060a42a4f380e8115ea
skd-alt <sipho@skanalytics.co.za>
2023-04-11 15:49:15 -0700

    allocate grid 0

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -8,6 +8,7 @@
  * If width or height is 0 or negative, return NULL.
  * The function should return NULL on failure
  */
+
 int **alloc_grid(int width, int height)
 {
 	int i, j, **ptr;
@@ -15,11 +16,22 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	ptr = malloc((width * height) * sizeof(int *));
+	ptr = (int **) malloc(height * sizeof(int *));
 
 	if (ptr == NULL)
 		return (NULL);
 
+	for (i = 0; i < height; i++)
+	{
+		ptr[i] = (int *) malloc(width * sizeof(int));
+		if (ptr[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(ptr[i]);
+			free(ptr);
+			return (NULL);
+		}
+	}
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
 			ptr[i][j] = 0;
