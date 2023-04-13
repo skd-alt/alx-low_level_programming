071ba3e1502466c663c86c9e90dba3b13e0bf9e4
skd-alt <sipho@skanalytics.co.za>
2023-04-13 13:36:10 -0700

    malloc check

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Allocates memory using malloc.
+ * @b: size of memory to be allocated.
+ *
+ * Return: pointer to allocated memory.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
