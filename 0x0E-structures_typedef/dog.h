e0a7d1be46df2cfe53904c8446c6ba888665d99d
skd-alt <sipho@skanalytics.co.za>
2023-04-17 13:33:14 -0700

    dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's info
+ * @name: First name
+ * @age: age
+ * @owner: Owner of dog
+ *
+ * Description: This struct is for dog infomation.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
