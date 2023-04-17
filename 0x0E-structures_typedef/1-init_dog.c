b24f9aad485bfd147045b7d4fce3b820f56ee95e
skd-alt <sipho@skanalytics.co.za>
2023-04-17 13:40:42 -0700

    dog struct function initialise

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+/**
+ * init_dog - initializes an object of type struct dog.
+ * @d: The dog to be initialized
+ * @name: The name of the dog
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
