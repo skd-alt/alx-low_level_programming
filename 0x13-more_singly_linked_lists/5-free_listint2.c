b402ef530a748141231149621c2ea66f537ea533
skd-alt <sdingani@yahoo.com>
2023-05-03 10:06:17 -0700

    Free list and set head to null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees list of int and sets head to Null
+ * @head: pointer to first node
+ *
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp_node;
+
+	while (*head)
+	{
+		tmp_node = (*head)->next;
+		free(*head);
+		*head = tmp_node;
+	}
+
+	*head = NULL;
+
+}
