9204089d6eb527d6508bf707d359672e9678b39b
skd-alt <sdingani@yahoo.com>
2023-05-08 16:24:50 -0700

    Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,9 +24,12 @@ int create_file(const char *filename, char *text_content)
 	if (fd < 0)
 		return (-1);
 
-	w_letters = write(fd, text_content, letters);
-	if (w_letters < 0)
-		return (-1);
+	if (text_content != NULL)
+	{
+		w_letters = write(fd, text_content, letters);
+		if (w_letters < 0)
+			return (-1);
+	}
 
 	close(fd);
 	return (1);
