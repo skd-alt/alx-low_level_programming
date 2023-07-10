c2357587f90492e0f14c23b94e81f9fa91ef6c9a
skd-alt <sipho@skanalytics.co.za>
2023-07-10 12:31:36 -0700

    Write to a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file - Create a file
+ * @filename: name of file
+ * @text_content: text content
+ *
+ * Return: 1 for sucess and -1 for fail
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
