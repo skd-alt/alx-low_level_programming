f4a4130e0bec9712d5b7fe6c6beed2dd6acc1273
skd-alt <sipho@skanalytics.co.za>
2023-09-08 14:25:03 -0700

    add values

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,52 @@
+#include "hash_tables.h"
+
+/**
+ * add_hash_node - adds a node at the beginning of hash index
+ * @h: head of list
+ * @key: key
+ * @value: value to be inputted
+ *
+ * Return: head of list
+ */
+
+hash_node_t *add_h_node(hash_node_t **h, const char *key, const char *value)
+{
+	hash_node_t *temp;
+
+	temp = malloc(sizeof(hash_node_t));
+	if (temp == NULL)
+		return (NULL);
+	temp->key = strdup(key);
+	temp->value = strdup(value);
+	temp->next = *h;
+	*h = temp;
+
+	return (*h);
+}
+
+/**
+ * hash_table_set - adds an element to the hash table.
+ * @ht: hash table pointer
+ * @key: is the key
+ * @value: is the value being entered
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int idx;
+
+	if (ht == NULL)
+		return (0);
+
+	if (key == NULL || *key == '\0')
+		return (0);
+
+	idx = key_index((unsigned char *)key, ht->size);
+
+	if (add_h_node(&(ht->array[idx]), key, value) == NULL)
+		return (0);
+
+	return (1);
+}
