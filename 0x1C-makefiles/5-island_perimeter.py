6a2b28a554ff8929e6aa71c9ecefd8991c7058cb
skd-alt <sipho@skanalytics.co.za>
2023-10-25 14:50:30 -0700

    Island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,54 +1,26 @@
 #!/usr/bin/python3
-"""Calculates the perimeter of an Island"""
+""" Module that contains the island_perimeter function """
 
 
 def island_perimeter(grid):
-    """Function to calculate perimeter
-        grid : Nested list represnting the island
-    """
+    """ Function that returns the perimeter of island described in grid """
+    perimeter = 0
 
     num_rows = len(grid)
 
     if grid != []:
         num_cols = len(grid[0])
-    
-    perimeter = 0
 
     for i in range(num_rows):
         for j in range(num_cols):
             if grid[i][j] == 1:
-                count_ones = check_ones(i, j, grid)
-                if count_ones < 3:
-                    perimeter += 3
-                else:
-                    perimeter += 2
+                if (i - 1) < 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                if (i + 1) == num_rows or grid[i + 1][j] == 0:
+                    perimeter += 1
+                if (j - 1) < 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                if (j + 1) == num_cols or grid[i][j + 1] == 0:
+                    perimeter += 1
 
     return perimeter
-
-                    
-def check_lower(a):
-    if a > 0:
-        return a - 1
-    elif a == 0:
-        return a
-
-def check_upper(a, grid):
-    if a < len(grid) - 1:
-        return a + 2
-    else:
-        return a + 1
-
-def check_ones(a, b, grid):
-    """Check to add 2 or 3"""
-
-    count = 0
-    sa = check_lower(a)
-    ea = check_upper(a, grid)
-    sb = check_lower(b)
-    eb = check_upper(b, grid[0])
-    for i in range(sa, ea):
-        for j in range(sb, eb):
-            if grid[i][j] == 1 and count < 3:
-                count += 1
-
-    return count
